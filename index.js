111750c6851e2e0cf4b4da78a3fb46f7f6469326
Samuraikiller <nurikmirr2005@gmail.com>
2023-12-26 13:59:19 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// Cat
+const cat = {
+    name: "Шарик",
+    age: 4,
+};
+console.log(cat);
+// Car
+const car = {
+    model: "Gt-r",
+    year: 2022,
+};
+console.log(car);
+car.year = 2020;
+console.log(car);
+
+// Book
+const book = {
+    name: "Headway",
+    author: "Tom Jerry",
+    year: 2015,
+};
+console.log(book);
+book.genre = "Study"
+console.log(book);
+
+// Student
+const student = {
+    name: "Ivan",
+    age: 21,
+    average: 4.5,
+};
+console.log(student);
+student.average = 5
+console.log(student);
+
+//fruits
+const fruits = {
+    name: "apple",
+    color: "red",
+    taste: "delicious"
+};
+console.log(fruits);
+fruits.score = 10
+console.log(fruits)
